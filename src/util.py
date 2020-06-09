ce1b92088149eb241cc87ff71cdd0d25cca0a5b9
Norme <antonnormelius@MacBook-Pro.local>
2020-06-09 16:34:16 +0200

    Change typos

--- src/util.py
+++ src/util.py
@@ -1,13 +1,16 @@
-
-
-
+"""
+@ 2020, Anton Normelius.
+Simple file transfer library between client and server,
+based on the socket library.
+MIT License.
+"""
 
 class Item():
     """
     Class to contain a single item,
     which is represented as a file or a folder.
     """
-    def __inti__(self):
+    def __init__(self):
         self.name = None
         self.path = None
         self.content = None
