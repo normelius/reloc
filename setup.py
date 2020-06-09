ab873e1be9935942111be5d25abc24f63db63041
Norme <antonnormelius@MacBook-Pro.local>
2020-06-09 11:47:10 +0200

    Changed name

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-norme",
+    name="trado",
     version="0.0.1",
     author="Anton Normelius",
     author_email="a.normelius@gmail.com",
-    description="Package to seamlessly transfer files and folders between client and server",
+    description="Simple file transfer between client and server",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/normelius/trado",
@@ -18,5 +18,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6', # CHECK THIS BY INSTALLING DIFFERENT PYTHON
+    python_requires='>=3.6',
 )
