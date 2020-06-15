a217d0dc0c6dee73a08dbde8befce46d5c9d0924
Norme <antonnormelius@MacBook-Pro.local>
2020-06-15 13:32:41 +0200

    Changed structure of setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,41 @@
 import setuptools
+from distutils.util import convert_path
+
+NAME = "reloc"
+AUTHOR = "Anton Normelius"
+EMAIL = "a.normelius@gmail.com"
+DESCRIPTION = "Package for simple file transfer between client and server"
+URL = "https://github.com/normelius/reloc"
+PACKAGES = ['reloc']
+PYTHON_REQUIRES = ">=3.6"
+
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+# Read latest version
+VER = {}
+version_path = convert_path("reloc/__version__.py")
+with open(version_path) as version_file:
+    exec(version_file.read(), VER)
+
+
 setuptools.setup(
-    name="reloc-normelius",
-    version="0.0.1",
-    author="Anton Normelius",
-    author_email="a.normelius@gmail.com",
-    description="Simple file transfer between client and server",
+    name=NAME,
+    version=VER['__version__'],
+    author=AUTHOR,
+    author_email=EMAIL,
+    description=DESCRIPTION,
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/normelius/reloc",
-    packages=['reloc'],
+    url=URL,
+    packages=PACKAGES,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires=PYTHON_REQUIRES,
     entry_points = {
         'console_scripts': ['reloc=reloc.cli:cli_transmit'],
     }
